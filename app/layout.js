1d622ad8ad29dee0294711d4984a87750452c096
Hasnain190 <s20755753@gmail.com>
2026-09-12 11:39:56 +0500

    Add Google Tag Manager scripts for enhanced tracking and analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import './globals.css';
 import { Inter, Noto_Nastaliq_Urdu } from 'next/font/google';
 import Footer from './components/footer';
 import Floating from './components/floating';
+import Script from 'next/script';
 const inter = Inter({ subsets: ['latin'] });
 const urduFont = Noto_Nastaliq_Urdu({
     subsets: ['arabic'],
@@ -34,6 +35,18 @@ export default function RootLayout({ children }) {
         <html lang="en">
 
             <body className={`${inter.className} ${urduFont.variable}`}>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-11062936395"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-ads-gtag" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'AW-11062936395');
+                    `}
+                </Script>
                 <Navbar />
                 {children}
                 <Floating href={WhatsappLink} />
